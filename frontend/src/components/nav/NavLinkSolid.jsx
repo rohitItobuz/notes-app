43d1510cb27cd09193e13ee9ca08aaa31c633704
Rohit Kheto <rohit@itobuz.com>
2025-02-14 16:24:40 +0530

    fix : email verification button color

--- frontend/src/components/nav/NavLinkSolid.jsx
+++ frontend/src/components/nav/NavLinkSolid.jsx
@@ -4,7 +4,20 @@ export const NavLinkSolid = ({ text, to, bg = "blue" }) => {
   return (
     <NavLink
       to={to}
-      className={`text-white cursor-pointer w-fit py-3 px-5 bg-${bg}-700 hover:bg-${bg}-800 transition-colors duration-300 font-bold rounded-3xl`}
+      className={`text-white cursor-pointer w-fit py-3 px-5 ${
+        bg === "red"
+          ? "bg-red-700"
+          : bg === "green"
+          ? "bg-green-600"
+          : "bg-blue-600"
+      } 
+      ${
+        bg === "red"
+          ? "hover:bg-red-800"
+          : bg === "green"
+          ? "hover:bg-green-800"
+          : "hover:bg-blue-800"
+      } transition-colors duration-300 font-bold rounded-3xl`}
     >
       {text}
     </NavLink>
